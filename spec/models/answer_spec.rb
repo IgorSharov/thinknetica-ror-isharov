26a1fe38f88d170058d9bd9d215c318a1b01ef6f
Igor Sharov <IgorSharov@users.noreply.github.com>
2017-08-09 20:28:43 +0300

    Added Shoulda-matchers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,11 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it 'validates title presence' do
-    expect(Answer.new(body: 'otvet')).to_not be_valid
-  end
-
-  it 'validates body presence' do
-    expect(Answer.new(title: 'zagolovok')).to_not be_valid
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,11 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it 'validates title presence' do
-    expect(Question.new(body: 'vopros')).to_not be_valid
-  end
-
-  it 'validates body presence' do
-    expect(Question.new(title: 'zagolovok')).to_not be_valid
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 end
