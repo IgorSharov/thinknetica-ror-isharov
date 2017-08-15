4fa86606faa5144ec52ffe9676fd0adeef96ac49
Igor Sharov <IgorSharov@users.noreply.github.com>
2017-08-15 23:16:05 +0300

    Added QuestionsController#index spec

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
 end
 
 group :test do
+  gem 'rails-controller-testing'
   gem 'shoulda-matchers'
 end
 
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 FactoryGirl.define do
+  sequence :title do |n|
+    "Title#{n}"
+  end
+  sequence :body do |n|
+    "Body#{n}"
+  end
+
   factory :question do
-    title 'MyString'
-    body 'MyText'
+    title
+    body
   end
 end
