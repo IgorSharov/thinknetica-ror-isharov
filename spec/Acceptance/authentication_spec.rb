13b58c503db1ee450f5b5117eb030f947f68ae61
Igor Sharov <IgorSharov@users.noreply.github.com>
2017-08-24 22:02:03 +0300

    Added spec for unregistered user to sign up

--- spec/Acceptance/authentication_spec.rb
+++ spec/Acceptance/authentication_spec.rb
@@ -31,3 +31,22 @@ RSpec.feature 'User signs in', '
     expect(page).to have_current_path new_user_session_path
   end
 end
+
+RSpec.feature 'Unregistered user can sign up', '
+  In order to sign up
+  as an unregistered user
+  I want to create a new user account
+' do
+  scenario 'Unregistered user creates a new user account' do
+    visit new_user_registration_path
+
+    email = 'new_user@test.com'
+
+    fill_in 'Email', with: email
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
