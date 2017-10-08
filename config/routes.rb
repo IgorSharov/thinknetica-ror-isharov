020607a6a8218b8cd7cd72fcc3e1b424036fda2f
Igor Sharov <IgorSharov@users.noreply.github.com>
2017-10-08 19:37:59 +0300

    Amended routes for votes via concerns

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :questions do
-    resources :answers do
+  concern :votable do
+    resources :votes, only: :create
+  end
+
+  resources :questions, concerns: :votable do
+    resources :answers, concerns: :votable do
       patch 'best_answer', on: :member
     end
   end
 
   resources :attachments, only: :destroy
-
-  post '/votes/:votable_type/:votable_id/:value', to: 'votes#create', as: :vote
 
   root to: 'questions#index'
 end
