22b1ea142ef43edb7f7a64c93fa700f68b5eb938
Igor Sharov <IgorSharov@users.noreply.github.com>
2018-08-22 19:44:46 +0300

    AcceptanceHelper win_remote_fixture_path

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -7,4 +7,11 @@ module AcceptanceHelper
     fill_in 'Password', with: user.password
     click_on 'Log in'
   end
+
+  def win_remote_fixture_path(file_name)
+    File.join('D:',
+              'Development',
+              'thinknetica-ror-isharov',
+              file_fixture(file_name)).tr!('/', '\\')
+  end
 end
