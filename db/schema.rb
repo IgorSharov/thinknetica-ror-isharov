9777622a1fe32e2658db2834a78700666702e3d4
Igor Sharov <IgorSharov@users.noreply.github.com>
2018-08-27 17:44:01 +0300

    Doorkeeper add confidential

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124105857) do
+ActiveRecord::Schema.define(version: 2018_08_27_143834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20171124105857) do
     t.string 'scopes', default: '', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.boolean 'confidential', default: true, null: false
     t.index ['uid'], name: 'index_oauth_applications_on_uid', unique: true
   end
 
